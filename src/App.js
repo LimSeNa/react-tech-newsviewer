118e9ef76f8a61318a82ad5ab558e7804bddd813
Sena Lim <tpsk8322@naver.com>
2023-05-05 18:03:21 +0900

    뉴스뷰어 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import axios from "axios";
-import {useState} from "react";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-    const [data, setData] = useState(null);
-    const onClick = async () => {
-        try {
-            const response = await axios.get(
-                'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6811653db7a1441bb0b172ec7ec0ed69',
-            );
-            setData(response.data);
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
     return (
-        <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-        </div>
+        <NewsList/>
     );
 };
 

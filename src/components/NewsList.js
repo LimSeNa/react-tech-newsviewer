8307aa72b28ffab63aa5086c0641321b0502e536
Sena Lim <tpsk8322@naver.com>
2023-05-07 00:37:36 +0900

    props로 받아 온 category에 따라 카테고리를 지정하여 API 호출하기

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -25,8 +25,9 @@ const NewsList = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`;
                 const response = await axios.get(
-                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6811653db7a1441bb0b172ec7ec0ed69',
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=e708c1a62cc049afb24b8e04ec5d016f`,
                 );
                 setArticles(response.data.articles);
             } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
             setLoading(false);
         };
         fetchData();
-    },[]);
+    },[category]);
 
     // 대기 중일 때
     if (loading) {
